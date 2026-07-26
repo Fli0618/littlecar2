7dfc665028a759354cecfde9bea79cfc2ba19257
Tzufucius <952105672@qq.com>
2026-07-26 20:51:13 +0800

    App_RunTask视觉测试部分

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,6 +128,87 @@ static void App_TryResetWorldOrigin(void)
 static void App_RunTask(void)
 {
   /* 后续比赛流程在此按顺序调用 Blocking 高级接口。 */
+
+
+  char code[DETECT_QR_CODE_LENGTH + 1U] = {0};
+  Detect_TargetList_t targets = {0};
+  uint8_t i;
+
+  /* 1. 二维码识别 */
+  (void)detect_qr_read_blocking(code);
+  printf("[QR] %s\r\n", code);
+
+  /*
+   * detect_qr_read_blocking() 退出前已经发送 STOP。
+   * 稍等 USART6 DMA 发送完成，再启动下一项任务。
+   */
+  HAL_Delay(10U);
+
+  /* 2. 颜色识别 */
+  (void)detect_color_start();
+
+  while (1)
+  {
+    if (detect_get_targets(&targets) != 0U)
+    {
+      printf("[COLOR] count=%u\r\n",
+             (unsigned int)targets.count);
+
+      for (i = 0U; i < targets.count; ++i)
+      {
+        printf("type=%u x=%d y=%d conf=%u\r\n",
+               (unsigned int)targets.targets[i].type,
+               (int)targets.targets[i].x,
+               (int)targets.targets[i].y,
+               (unsigned int)targets.targets[i].confidence);
+      }
+
+      /* 检测到至少一个目标后进入下一阶段 */
+      if (targets.count > 0U)
+      {
+        break;
+      }
+    }
+
+    __WFI();
+  }
+
+  (void)detect_stop();
+  HAL_Delay(10U);
+
+  /* 清空上一个阶段的本地变量 */
+  targets = (Detect_TargetList_t){0};
+
+  /* 3. 数字圆环识别 */
+  (void)detect_circle_start();
+
+  while (1)
+  {
+    if (detect_get_targets(&targets) != 0U)
+    {
+      printf("[CIRCLE] count=%u\r\n",
+             (unsigned int)targets.count);
+
+      for (i = 0U; i < targets.count; ++i)
+      {
+        printf("type=%u x=%d y=%d conf=%u\r\n",
+               (unsigned int)targets.targets[i].type,
+               (int)targets.targets[i].x,
+               (int)targets.targets[i].y,
+               (unsigned int)targets.targets[i].confidence);
+      }
+
+      if (targets.count > 0U)
+      {
+        break;
+      }
+    }
+    __WFI();
+  }
+  (void)detect_stop();
+  printf("[TEST] finished\r\n");
+
+  
 }
 
 static void App_TimerUpdate(void)
@@ -160,16 +241,16 @@ static void App_TimerUpdate(void)
 
     switch (AdvanceControl_GetMode())
     {
-      case ADVANCE_CONTROL_WORLD:
-        AdvanceMotion_Update();
-        break;
+    case ADVANCE_CONTROL_WORLD:
+      AdvanceMotion_Update();
+      break;
 
-      case ADVANCE_CONTROL_VISUAL:
-        AdvanceVisual_Update();
-        break;
+    case ADVANCE_CONTROL_VISUAL:
+      AdvanceVisual_Update();
+      break;
 
-      default:
-        break;
+    default:
+      break;
     }
   }
 
@@ -189,9 +270,9 @@ static void App_TimerUpdate(void)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -231,7 +312,8 @@ int main(void)
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
     HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_9 | GPIO_PIN_10);
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-    for(volatile int j=0; j<300000; j++); // 缩短延时，亮暗更明显
+    for (volatile int j = 0; j < 300000; j++)
+      ; // 缩短延时，亮暗更明显
   }
   /* USER CODE END Init */
 
@@ -311,22 +393,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -341,9 +423,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
@@ -356,10 +437,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief TIM6 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM6 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM6_Init(void)
 {
 
@@ -390,14 +471,13 @@ static void MX_TIM6_Init(void)
   /* USER CODE BEGIN TIM6_Init 2 */
 
   /* USER CODE END TIM6_Init 2 */
-
 }
 
 /**
-  * @brief UART4 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief UART4 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_UART4_Init(void)
 {
 
@@ -423,14 +503,13 @@ static void MX_UART4_Init(void)
   /* USER CODE BEGIN UART4_Init 2 */
 
   /* USER CODE END UART4_Init 2 */
-
 }
 
 /**
-  * @brief UART5 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief UART5 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_UART5_Init(void)
 {
 
@@ -456,14 +535,13 @@ static void MX_UART5_Init(void)
   /* USER CODE BEGIN UART5_Init 2 */
 
   /* USER CODE END UART5_Init 2 */
-
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
 
@@ -489,14 +567,13 @@ static void MX_USART1_UART_Init(void)
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -522,14 +599,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART3_UART_Init(void)
 {
 
@@ -555,14 +631,13 @@ static void MX_USART3_UART_Init(void)
   /* USER CODE BEGIN USART3_Init 2 */
 
   /* USER CODE END USART3_Init 2 */
-
 }
 
 /**
-  * @brief USART6 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART6 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART6_UART_Init(void)
 {
 
@@ -588,12 +663,11 @@ static void MX_USART6_UART_Init(void)
   /* USER CODE BEGIN USART6_Init 2 */
 
   /* USER CODE END USART6_Init 2 */
-
 }
 
 /**
-  * Enable DMA controller clock
-  */
+ * Enable DMA controller clock
+ */
 static void MX_DMA_Init(void)
 {
 
@@ -620,14 +694,13 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream6_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream6_IRQn, 4, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream6_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -645,17 +718,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9 | GPIO_PIN_10, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PF9 PF10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LIFT_UP_LIMIT_Pin LIFT_DOWN_LIMIT_Pin SLIDE_FRONT_LIMIT_Pin */
-  GPIO_InitStruct.Pin = LIFT_UP_LIMIT_Pin|LIFT_DOWN_LIMIT_Pin|SLIDE_FRONT_LIMIT_Pin;
+  GPIO_InitStruct.Pin = LIFT_UP_LIMIT_Pin | LIFT_DOWN_LIMIT_Pin | SLIDE_FRONT_LIMIT_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
@@ -702,7 +775,6 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
   {
     CommJetson_OnUartRxEvent(huart, Size);
   }
-
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
@@ -716,7 +788,6 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
   {
     CommJetson_OnUartTxComplete(huart);
   }
-
 }
 
 void HAL_UART_AbortTransmitCpltCallback(UART_HandleTypeDef *huart)
@@ -753,7 +824,6 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
   {
     CommJetson_OnUartError(huart);
   }
-
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
@@ -767,9 +837,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -782,12 +852,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
